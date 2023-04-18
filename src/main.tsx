d0c71f3ae40815b4a97ba82e4a59a973ba4f1f92
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-18 13:48:23 +0100

    remove context

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-import { ReviewContextProvider } from './store/review-context';
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ReviewContextProvider>
-      <App />
-    </ReviewContextProvider>
+    <App />
   </React.StrictMode>
 );
