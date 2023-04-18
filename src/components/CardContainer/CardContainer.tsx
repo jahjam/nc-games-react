da8f7f190d97fc5ea3fa8c6a8b2979de10d1c965
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-18 13:48:40 +0100

    impliment custom hook over context

--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -1,28 +1,52 @@
 import * as Styled from './styles';
 
 import ReviewCard from '../ReviewCard/ReviewCard';
-import { useContext } from 'react';
-import { ReviewContext } from '../../store/review-context';
+import { useEffect, useState } from 'react';
+import { useRequest } from '../../hooks/use-request';
+
+type Review = {
+  title: string;
+  category: string;
+  designer: string;
+  owner: string;
+  review_img_url: string;
+  created_at: Date;
+  votes: number;
+  comment_count: number;
+};
 
 const CardContainer = () => {
-  const reviewContext = useContext(ReviewContext);
+  const [reviews, setReviews] = useState<Array<Review>>([]);
 
-  const { reviews } = reviewContext;
+  const { sendRequest, isError, isLoading, errorMsg } = useRequest();
+
+  useEffect(() => {
+    const res = (data: Array<Review>) => {
+      setReviews(data);
+    };
+
+    sendRequest('GET', '/reviews', res);
+  }, []);
 
   return (
     <Styled.Container direction="column">
-      {reviews.map((review, i) => (
-        <ReviewCard
-          key={i}
-          title={review.title}
-          designer={review.designer}
-          owner={review.owner}
-          createdAt={new Date(review.created_at)}
-          numComments={review.comment_count}
-          votes={review.votes}
-          img={review.review_img_url}
-        />
-      ))}
+      {isError && <span>{errorMsg}</span>}
+      {isLoading && !isError ? (
+        <span>Loading...</span>
+      ) : (
+        reviews.map((review, i) => (
+          <ReviewCard
+            key={i}
+            title={review.title}
+            designer={review.designer}
+            owner={review.owner}
+            createdAt={new Date(review.created_at)}
+            numComments={review.comment_count}
+            votes={review.votes}
+            img={review.review_img_url}
+          />
+        ))
+      )}
     </Styled.Container>
   );
 };
