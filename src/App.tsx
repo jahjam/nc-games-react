c51a87fcde907e06bdfd06533cb0132a26ec41d3
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-19 09:18:29 +0100

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import * as Styled from './styles';
 
 import Header from './feature/Header/Header';
 import Home from './pages/Home/Home';
+import Review from './pages/Review/Review';
+import Footer from './feature/Footer/Footer';
+
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <Styled.App>
+    <Styled.App direction="column" justify="flex-start">
       <Header />
 
-      <Home />
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/reviews" />} />
+        <Route path="/reviews" element={<Home />} />
+        <Route path=":reviewId" element={<Review />} />
+      </Routes>
+
+      <Footer />
     </Styled.App>
   );
 }
