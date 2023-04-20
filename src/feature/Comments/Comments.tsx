09862b0afee8c1b0e3338e77b977cb1a37b1cc28
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-20 18:47:00 +0100

    add elements

--- src/feature/Comments/Comments.tsx
+++ src/feature/Comments/Comments.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { useRequest } from '../../hooks/use-request';
 import * as Styled from './styles';
 
 import { Comment } from '../../types/types';
 import CommentCard from '../../components/CommentCard/CommentCard';
+import { Button } from '../../components/Button/styles';
+import AuthContext from '../../store/auth-context';
 
 type Props = {
   reviewId: number;
@@ -13,9 +15,18 @@ type ResponseT = {
   comments: Array<Comment>;
 };
 
+type PostResponseT = {
+  comment: Array<Comment>;
+};
+
 const Comments = ({ reviewId }: Props) => {
+  const body = useRef<HTMLTextAreaElement>(null);
   const [comments, setComments] = useState<Array<Comment>>();
   const { sendRequest, isError, isLoading, errorMsg } = useRequest();
+
+  const authContext = useContext(AuthContext);
+
+  const { isLoggedIn, userDetails } = authContext;
 
   useEffect(() => {
     const res = (data: ResponseT) => {
@@ -25,12 +36,51 @@ const Comments = ({ reviewId }: Props) => {
     sendRequest('GET', `/reviews/${reviewId}/comments`, res);
   }, []);
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const res = (data: PostResponseT) => {
+      setComments(prevComments => {
+        return [...data.comment, ...(prevComments as Comment[])];
+      });
+
+      body!.current!.value = '';
+    };
+
+    const reqBody = {
+      username: userDetails.username,
+      body: body.current?.value,
+    };
+
+    sendRequest('POST', `/reviews/${reviewId}/comments`, res, reqBody);
+  };
+
   return (
     <Styled.Comments direction="column">
-      {comments && !comments.length && (
-        <span>No comments for this reviews yet!</span>
+      {isLoggedIn ? (
+        <>
+          <h3>Add A Comment</h3>
+          <Styled.Form onSubmit={handleSubmit} direction="column" gap={0.8}>
+            <label htmlFor="body">Comment:</label>
+            <textarea ref={body} id="body"></textarea>
+            {isError && <span>{errorMsg}</span>}
+            {isLoading ? (
+              <Button type="button">Loading...</Button>
+            ) : (
+              <Button type="submit">Add Comment</Button>
+            )}
+          </Styled.Form>
+        </>
+      ) : (
+        <span>Please sign in to leave a comment</span>
       )}
+
+      {comments && !comments.length && (
+        <span>No comments for this reviews yet</span>
+      )}
+
       {isLoading && <span>Loading...</span>}
+
       {comments &&
         comments.map((comment, i) => (
           <CommentCard
@@ -41,6 +91,7 @@ const Comments = ({ reviewId }: Props) => {
             votes={comment.votes}
           />
         ))}
+
       {isError && <span>{errorMsg}</span>}
     </Styled.Comments>
   );
