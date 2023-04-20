17488794ffd33add4c177ea8773c812e829d2054
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-20 18:47:07 +0100

    add auth

--- src/feature/Header/Header.tsx
+++ src/feature/Header/Header.tsx
@@ -1,25 +1,43 @@
 import { useNavigate } from 'react-router-dom';
 import * as Styled from './styles';
+import { useContext } from 'react';
+import AuthContext from '../../store/auth-context';
 
 const Header = () => {
   const navigate = useNavigate();
 
-  const handleHeaderClick = () => {
+  const authContext = useContext(AuthContext);
+
+  const { isLoggedIn } = authContext;
+
+  const handleHeadingClick = () => {
     navigate('/');
+  };
+
+  const handleSignInAccountClick = () => {
+    navigate('/sign-in');
+  };
+
+  const handleAccountClick = () => {
+    navigate('/me');
   };
 
   return (
     <Styled.Header justify="space-between">
       <Styled.MenuIcon />
       <Styled.HeadingContainer direction="column" gap={0.2}>
-        <Styled.Heading onClick={handleHeaderClick}>
+        <Styled.Heading onClick={handleHeadingClick}>
           Rer
           <Styled.DiceIcon />
           ll
         </Styled.Heading>
         <span>board game reviews</span>
       </Styled.HeadingContainer>
-      <Styled.AccountIcon />
+      {isLoggedIn ? (
+        <Styled.AccountIcon onClick={handleAccountClick} />
+      ) : (
+        <Styled.AccountLoginIcon onClick={handleSignInAccountClick} />
+      )}
     </Styled.Header>
   );
 };
