259e37518916ffe052fdea208548f5f2dc6e1e05
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-23 23:15:55 +0100

    add wrong path page

---
+++ src/pages/WrongPath/WrongPath.tsx
@@ -0,0 +1,11 @@
+import * as Styled from './styles';
+
+const WrongPath = () => {
+  return (
+    <Styled.WrongPath>
+      <span>Sorry this page doesn't exist!</span>
+    </Styled.WrongPath>
+  );
+};
+
+export default WrongPath;
