467aa62808c26e4dc8c2cb1f7c727e6c32f55d7b
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-23 13:21:53 +0100

    refactor

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,41 +1,11 @@
-import CardContainer from '../../components/CardContainer/CardContainer';
-import Filter from '../../feature/Filter/Filter';
 import * as Styled from './styles';
 
-import { useEffect, useState } from 'react';
-import { useRequest } from '../../hooks/use-request';
-
-import { Review } from '../../types/types';
-
-type ResponseT = {
-  reviews: Array<Review>;
-};
+import Reviews from '../../feature/Reviews/Reviews';
 
 const Home = () => {
-  const [reviews, setReviews] = useState<Array<Review>>([]);
-
-  const { sendRequest, isError, isLoading, errorMsg } = useRequest();
-
-  useEffect(() => {
-    const res = (data: ResponseT) => {
-      setReviews(data.reviews);
-    };
-
-    sendRequest('GET', '/reviews', res);
-  }, []);
-
   return (
     <Styled.Home direction="column" gap={1}>
-      <Filter />
-
-      <h2>All Reviews</h2>
-
-      <CardContainer
-        reviews={reviews}
-        isError={isError}
-        isLoading={isLoading}
-        errorMsg={errorMsg}
-      />
+      <Reviews />
     </Styled.Home>
   );
 };
