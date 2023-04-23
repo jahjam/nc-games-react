0138c63c1c846525cfa6639f43860ff1b71e6a5e
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-23 23:12:32 +0100

    handle comment delete

--- src/components/CommentCard/CommentCard.tsx
+++ src/components/CommentCard/CommentCard.tsx
@@ -1,11 +1,37 @@
 import * as Styled from './styles';
 import { Comment } from '../../types/types';
 import { format } from 'date-fns';
+import { useRequest } from '../../hooks/use-request';
 
-const CommentCard = ({ author, body, date, votes }: Comment) => {
+const CommentCard = ({
+  author,
+  body,
+  date,
+  votes,
+  comment_id: id,
+  handleDelete,
+}: Comment) => {
+  const { sendRequest, isError, isLoading } = useRequest();
+
+  const handleBinClick = () => {
+    const res = () => {
+      handleDelete(id);
+    };
+
+    sendRequest('DELETE', `/comments/${id}`, res);
+  };
+
   return (
     <Styled.CommentCard direction="column" gap={0.8}>
-      <h3>{author}</h3>
+      {isError && (
+        <Styled.Error>
+          Oops, something went wrong. Please try again.
+        </Styled.Error>
+      )}
+      <div>
+        <h3>{author}</h3>
+        {isLoading ? <span>...</span> : <Styled.Bin onClick={handleBinClick} />}
+      </div>
       <p>{body}</p>
       <div>
         <span>{format(date as Date, 'dd/MM/yyyy')}</span>
